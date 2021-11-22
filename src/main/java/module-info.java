b69df88695fe751aac80e996cc1bd1184a805bc5
Layendan <layendan.h@gmail.com>
2021-11-22 15:40:25 -0700

    updated summary doc and requirements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module org.alabourd.honors_contract {
     requires javafx.controls;
+    requires transitive javafx.graphics;
 
     opens org.alabourd.honors_contract to javafx.fxml;
+
     exports org.alabourd.honors_contract;
     exports org.alabourd.honors_contract.Comparator;
+
     opens org.alabourd.honors_contract.Comparator to javafx.fxml;
 }
